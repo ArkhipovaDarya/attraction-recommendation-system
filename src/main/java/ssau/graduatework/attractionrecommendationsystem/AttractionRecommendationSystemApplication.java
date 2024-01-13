9b5e63d567d2ec9026fe8b0e7712e7459afc9632
Дарья Архипова <darkh0802@gmail.com>
2024-01-13 16:46:02 +0400

    start the project

---
+++ src/main/java/ssau/graduatework/attractionrecommendationsystem/AttractionRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package ssau.graduatework.attractionrecommendationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttractionRecommendationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttractionRecommendationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ssau/graduatework/attractionrecommendationsystem/AttractionRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ssau.graduatework.attractionrecommendationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttractionRecommendationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
